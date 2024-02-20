9e943d4575677717e0b0acf34aa6de5585a094a9
huatree <wang.yaohua@foxmail.com>
2024-02-20 16:40:17 +0800

    feat(*): ✨  框架布局

---
+++ src/stores/debug.ts
@@ -0,0 +1,37 @@
+import { ref, type Ref, onMounted, onBeforeMount } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useEnvStore = defineStore('env', () => {
+  const debug = ref(true)
+  function toggle() {
+    debug.value = !debug.value
+  }
+
+  return { debug, toggle }
+})
+
+export type useTimeParams = {
+  silent?: boolean
+}
+export type useTimeReturn = {
+  time: Ref<string>
+}
+
+export const useTime = (params: useTimeParams): useTimeReturn => {
+  let timer: number
+  const { silent = true } = params
+  const time = ref(new Date().toLocaleString())
+  onMounted(() => {
+    if (!silent) {
+      timer = window.setInterval(() => {
+        time.value = new Date().toLocaleString()
+      }, 1000)
+    }
+  })
+  onBeforeMount(() => {
+    if (!silent && timer) {
+      clearInterval(timer)
+    }
+  })
+  return { time }
+}
