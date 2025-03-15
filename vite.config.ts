3fedb18bcf63d85597c63fe7ee7cf06414403eb8
huatree <wang.yaohua@foxmail.com>
2025-03-15 22:28:19 +0800

    ci(vite): 🎡  配置公共基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/material/',
   plugins: [
     visualizer({
       filename: './bundle-analysis.html'
